15f1f5fabc8b34df1e1df394d82c72327b2dc277
hoseong511 <thdghtjd115@naver.com>
2021-03-12 12:51:04 +0900

    update while문

--- main.py
+++ main.py
@@ -4,9 +4,34 @@
 # for i in arr:
 #     print( "%d " %i if i < x else "", end="")
 
+# import sys
+# n = int(input())
+# arr = [sys.stdin.readline() for i in range(n)]
+# for i in range(n):
+#     a, b =map(int, arr[i].rstrip().split())
+#     print("Case #%d: %d" %(i+1, a+b))
+# arr = list()
 import sys
-n = int(input())
-arr = [sys.stdin.readline() for i in range(n)]
-for i in range(n):
-    a, b =map(int, arr[i].rstrip().split())
-    print("Case #%d: %d" %(i+1, a+b))
+while(True):
+    arr.append(sys.stdin.readline().rstrip())
+    if arr[-1] =='':
+        arr.pop()
+        break
+for i in arr:
+    a,b = map(int, i.split())
+    print(a+b)
+
+# while(True):
+#     a,b = map(int, input().spllit())
+#     print(a+b)
+#     if
+
+
+
+# while (True):
+#     try:
+#         a, b = map(int,sys.stdin.readline().rstrip().split())
+#         print(a + b)
+#     except:
+#         break
+
