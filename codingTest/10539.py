b2b4ec32528131edfb2fd084bc8fd8cf317636ff
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-23 13:46:29 +0900

    update 10539(2)

--- codingTest/10539.py
+++ codingTest/10539.py
@@ -1,9 +1,14 @@
 n, arr = int(input()), list(map(int,input().split()))
-result = [data*(i+1) for i,data in enumerate(arr) ]
+A = [arr[0]]
+for i in range(1, n):
+  A.append(arr[i]*(i+1)-sum(A))  
+print(' '.join(list(map(str,A))))
+# result = [data*(i+1) for i,data in enumerate(arr) ]
 
-for idx,data in enumerate(result):
-  if idx == 0:
-    arr[idx] = data
-    continue
-  arr[idx] = data-result[idx-1]
-print(' '.join(list(map(str,arr))))
+# for idx,data in enumerate(result):
+#   if idx == 0:
+#     arr[idx] = data
+#     continue
+#   arr[idx] = data-result[idx-1]
+# print(' '.join(list(map(str,arr))))
+
