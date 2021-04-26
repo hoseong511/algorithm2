c9b53884fc8b34b39cdf494fc231db320a150006
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-04-26 16:05:04 +0900

    update BOJ-9037

---
+++ codingTest/9037.py
@@ -0,0 +1,18 @@
+import sys
+def candy_toss(student_num, candy_arr, result=0):  
+  if candy_arr.count(candy_arr[0]) == student_num:
+    return result  
+  else:    
+    arr = [ int(n/2 + candy_arr[i-1]/2) 
+          if i != 0 
+          else int(n/2) + int(candy_arr[N-(i+1)]/2) 
+          for i,n in enumerate(candy_arr)] # 사탕을 오른쪽으로 전달
+    arr = list(map(lambda x : int(x) if int(x) % 2 == 0 else int(x)+1, arr)) # 홀수개인 아이에게 사탕을 하나씩
+    result += 1
+    count = candy_toss(student_num, arr, result)
+    return count
+T= int(sys.stdin.readline())
+for _ in range(T):
+  N, arr = int(sys.stdin.readline()), list(map(lambda x : int(x) if int(x) % 2 == 0 else int(x)+1, sys.stdin.readline().split()))
+  a = candy_toss(N, arr)  
+  sys.stdout.write(f"{a}\n")
