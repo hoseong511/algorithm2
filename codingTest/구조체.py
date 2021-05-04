1284b644fef24f2cee748bbfb89821e5b0aaf791
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-04 12:01:21 +0900

    update codeup 구조체2

--- codingTest/구조체.py
+++ codingTest/구조체.py
@@ -4,15 +4,23 @@ for i in range(int(input())):
   table[arr[0]] = list(map(int,arr[1:]))
 arr = list(table.items())
 first = ''
-answer = ''
 for i in range(3):
   result = sorted(arr, key = lambda x: x[1][i], reverse=True)
   if i == 0:
     first = result[0][0]
-    answer = first
+    print(first, end=' ')
     continue
-  for idx, j in enumerate(result):
-    if j[0] == answer:
-      first += f' {idx+1}'
+  result = [ [stu[0],stu[1][i]] for idx, stu in enumerate(result) ]
+  tmp = result[0][1] # tmp는 가장높은점수
+  rank = 1
+  for idx,j in enumerate(result):
+    if tmp != j[1]:
+      tmp = j[1]
+      j[1] = idx+1
+      rank = idx+1
+    else:
+      j[1] = rank
+  
+    if j[0] == first : # 1과목 1등인 학생의 다른 과목 석차
+      print(j[1], end=' ')
 
-print(first)
