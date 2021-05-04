98c0ae9a5a20048625c3a69edcc8a0a271a00eac
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-04 12:49:33 +0900

    update @cache-memoization

--- codingTest/기초.py
+++ codingTest/기초.py
@@ -1,12 +1,11 @@
-# from functools import reduce
-# 고차함수 reduce
-def reduce(function, iterable, initializer=None):
-    it = iter(iterable)
-    if initializer is None:
-        value = next(it)
-    else:
-        value = initializer
-    for element in it:
-        value = function(value, element)
-    return value
-print(reduce(lambda x,y: x*y, map(float,(input().split())), 3))
+from functools import reduce,cache
+print(reduce(lambda x,y: x//y, map(int,input().split())))
+
+@cache # memoizaition
+def factorial(n):
+    return n * factorial(n-1) if n else 1
+
+print(factorial(10))      # no previously cached result, makes 11 recursive calls
+print(factorial(5))       # just looks up cached value result
+print(factorial(12))      # makes two new recursive calls, the other 10 are cached
+
