60979ce583c184f13153518c4981f55b2bc1d93c
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-04 12:29:57 +0900

    update 고차함수 reduce

--- codingTest/기초.py
+++ codingTest/기초.py
@@ -1,1 +1,12 @@
-[print(i,end='') for i in input().split()]
+# from functools import reduce
+# 고차함수 reduce
+def reduce(function, iterable, initializer=None):
+    it = iter(iterable)
+    if initializer is None:
+        value = next(it)
+    else:
+        value = initializer
+    for element in it:
+        value = function(value, element)
+    return value
+print(reduce(lambda x,y: x*y, map(float,(input().split())), 3))
