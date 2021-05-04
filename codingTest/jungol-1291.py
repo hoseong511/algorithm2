6398782fafccc729f96b4d301c3e609f41ebd94e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-05-04 17:59:27 +0900

    update

--- codingTest/jungol-1291.py
+++ codingTest/jungol-1291.py
@@ -4,16 +4,10 @@ while True:
     print("INPUT ERROR!")
     continue
   arr = [i for i in range(n,m-1 if n>m else m+1, -1 if n > m else 1)]
-  for a in arr:
-    for j in range(3):
-      [print(f'{a} * {i+1} = {" "+ str(a * (i+1)) if a * (i+1)<=9 else a * (i+1) }',
-              end='   ') 
-                for i in range(j*3,(j+1)*3)]
-      print('')
+
+  for j in range(9):
+    [print(f'{i} * {j+1} = {" "+ str(i * (j+1)) if i * (j+1)<=9 else i * (j+1) }',
+             end='   ') 
+              for i in arr]
     print('')
-  # for j in range(9):
-  #   [print(f'{i} * {j+1} = {" "+ str(i * (j+1)) if i * (j+1)<=9 else i * (j+1) }',
-  #            end='   ') 
-  #             for i in arr]
-  #   print('')
   break
---
+++ codingTest/기초2.py
@@ -0,0 +1,8 @@
+a, b = map(int,input().split())
+add = a + b
+diff = a - b
+mul = a * b
+fdiv = a // b
+mod = a % b
+div = a / b
+print(add, diff, mul, fdiv, mod, '%.2f'%div, sep='\n')
