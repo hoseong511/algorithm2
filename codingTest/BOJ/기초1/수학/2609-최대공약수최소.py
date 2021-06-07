2540265c82b72eb9d3883b341355cba62620f500
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-07 16:54:19 +0900

    update BOJ: 알고리즘 기초1, 2609-최대공약수최소공배수, 유클리드 호제법 과 최대공약수와 최소공배수의 관계

---
+++ codingTest/BOJ/기초1/수학/2609-최대공약수최소.py
@@ -0,0 +1,18 @@
+def gcd(n, m):
+  if n < m :
+    n, m = m, n
+  if m == 0:
+    return n
+  if n % m == 0:
+    return m
+  else:
+    return gcd( m , n % m )
+
+n,m = map(int, input().split())
+a = gcd(n,m)
+b = a * n/a * m/a 
+print(a)
+print(int(b))
+# 24 , 18 , 6
+# 18 , 6 , 0
+# 6 , 0 , 6
