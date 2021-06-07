4c7f50d773ac86d0f0ee39b71c83ea7eef0fec45
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-07 14:15:41 +0900

    Create BOJ: 알고리즘 기초1, 10828-스택

--- codingTest/기초5.py
+++ codingTest/기초5.py
@@ -1,8 +1,25 @@
-a, m, d, n=map(int, input().split())
-i = 1
-while i!=n:
-  a *= m
-  a += d
-  i += 1
+graph = dict()
 
-print(a)
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+def dfs(graph, start_node):
+    visited, need_visit = list(), list()
+    need_visit.append(start_node)
+    
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+    
+    return visited
+dfs(graph, 'A')
