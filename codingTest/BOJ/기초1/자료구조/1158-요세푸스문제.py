f81fc359e23bb8f23a5a702a357a503839359b79
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-09 17:19:37 +0900

    UPdate BOJ: 알고리즘기초1, 1158, 큐, *list너무신기

---
+++ codingTest/BOJ/기초1/자료구조/1158-요세푸스문제.py
@@ -0,0 +1,16 @@
+N, K = map(int, input().split())
+queue, result = [i for i in range(1, N+1)], []
+
+for i in range(N):
+  if len(queue) < K:
+    tmp = K % len(queue)
+    queue = queue[tmp:] + queue[:tmp]
+    result.append(queue.pop())
+    continue
+  queue = queue[K:] + queue[:K]
+  result.append(queue.pop())
+# print('<', end='')
+# print(*result, sep = ', ', end='')
+# print('>', end='') # *result 이거 너무 신기해 
+result = map(str, result)
+print("<", ", ".join(result), ">", sep='')
