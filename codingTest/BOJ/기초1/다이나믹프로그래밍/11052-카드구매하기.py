5a2c1deaa26c7f8d2af9274c1835d53ea55d9c5e
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-10 22:53:36 +0900

    Update BOJ: 알고리즘기초1, solved 11052, DP

--- codingTest/BOJ/기초1/다이나믹프로그래밍/11052-카드구매하기.py
+++ codingTest/BOJ/기초1/다이나믹프로그래밍/11052-카드구매하기.py
@@ -4,17 +4,9 @@ price = map(int, sys.stdin.readline().split())
 price = [0] + list(price)
 memo = [0]*1001
 memo[1] = price[1]
-if N > 1:
-  memo[2] = max(price[1]+price[1], price[2])
-if N > 2:
-  memo[3] = max(price[1]+price[2], price[3], price[1]*3)
-if N <= 3:
-  print(memo[N])
-  exit(0)
-for i in range(4,N+1):
-  memo[i] = max(memo[i-1]+price[i-3],
-                memo[i-2]+price[i-2],
-                memo[i-3]+price[i-1])
-  print(memo)
+
+for i in range(2,N+1):
+  for j in range(1, i+1):
+    memo[i] = max(memo[i], memo[i-j]+price[j])
     
 print(memo[N])
