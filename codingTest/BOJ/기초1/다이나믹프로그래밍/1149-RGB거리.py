a1cc46cb938175de07539e936375e5a7614cedc7
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-11 15:29:10 +0900

    Update BOJ: 알고리즘기초1, solved 1149, DP

--- codingTest/BOJ/기초1/다이나믹프로그래밍/1149-RGB거리.py
+++ codingTest/BOJ/기초1/다이나믹프로그래밍/1149-RGB거리.py
@@ -1,14 +1,10 @@
 import sys
 N = int(sys.stdin.readline())
-memo = [0]*1001
-data = list(map(int,sys.stdin.readline().split()))
-memo[1] = min(data)
-idx = data.index(memo[1])
-for i in range(2, N+1):
-  data = list(map( int, sys.stdin.readline().split()))
-  tmp = data[:] # 복사
-  tmp.pop(idx)
-  memo[i] = memo[i-1] + min(tmp)
-  idx = data.index(min(tmp))
-  print(data ,idx)
-print(memo[N])
+memo = [[0,0,0] for _ in range(1001)]
+data = [0]+[list(map(int,sys.stdin.readline().split())) for _ in range(N)] 
+memo[1] = data[1]
+for i in range(2, N+1): # 각각의 경우(R,G,B)를 선택했을 때의 값을 저장하기
+  memo[i][0] = data[i][0] + min(memo[i-1][1], memo[i-1][2])
+  memo[i][1] = data[i][1] + min(memo[i-1][0], memo[i-1][2])
+  memo[i][2] = data[i][2] + min(memo[i-1][0], memo[i-1][1])
+print(min(memo[N]))
