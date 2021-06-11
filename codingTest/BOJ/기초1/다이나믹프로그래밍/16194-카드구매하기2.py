7959ab58210025220d69422f588a02eae87a0bed
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-11 17:18:12 +0900

    Update BOJ: 알고리즘기초1, 16194,DP

--- codingTest/BOJ/기초1/다이나믹프로그래밍/16194-카드구매하기2.py
+++ codingTest/BOJ/기초1/다이나믹프로그래밍/16194-카드구매하기2.py
@@ -6,7 +6,8 @@ memo = [0]*1001
 memo[1] = price[1]
 
 for i in range(2,N+1):
+  arr =[]
   for j in range(1, i+1):
-    memo[i] = min(memo[i], memo[i-j]+price[j])
-    
+    arr.append(memo[i-j] + price[j]) 
+  memo[i] = min(arr)    
 print(memo[N])
