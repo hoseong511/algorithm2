c7790cfd02cb88ee6787bd65c12c2a2c0b72466c
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-12 14:48:36 +0900

    UPdate BOJ: 알고리즘 기초1, solved 6588, 에라토스테네스의 체

--- codingTest/BOJ/기초1/수학/6588-골드바흐의추측.py
+++ codingTest/BOJ/기초1/수학/6588-골드바흐의추측.py
@@ -3,22 +3,40 @@ from timeit import default_timer as timer
 from datetime import timedelta
 
 start = timer()
+def prime_list(n):
+  primes = [True] * n
+  m = int(n ** 0.5)
+  for i in range(2, m + 1):
+    if primes[i] == True:
+      for j in range(i+i, n, i):
+        primes[j] = False
+  return primes
+
+# def prime_check(n):
+#   check = True
+#   m = int(n ** (1/2))
+#   for i in range(2, m+1):
+#     if n % i == 0:
+#       check = False
+#       break
+#   return check
+primes = prime_list(1000001)
 while True:
   T = int(sys.stdin.readline())
-  data = [i for i in range(T+1, 2, -2)]
-  for i in data:
-    check = True
-    tmp = int(i**(1/2))
-    for s in range(2,tmp+1):
-      if i % s == 0:
-        check = False
-        break
-    if check:
-      if T - i >= 3:
+  for i in range(T-1, 2 , -2):
+    if primes[i] and primes[T-i]:
+      if T-i >= 3:
         print(f"{T} = {T-i} + {i}")
         break
+
   if T == 0:
     break
+'''
+기존 코드와의 가장 큰 차이점은 하나씩 숫자를 받을 때 마다 
+반복문을 도는 것이고, 
+지금은 일단 범위 내의 소수를 다 찾아놓고 조건을 만족하는 수를 골라내는 식
+'''
+
 
 end = timer()
 print(timedelta(seconds=end-start))
