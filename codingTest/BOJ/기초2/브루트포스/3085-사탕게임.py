6ca26725dfd45874fddae6068f678737bd98b649
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-14 16:37:23 +0900

    Update BOJ: 알고리즘기초2, 3085, 브루트포스, 방향벡터이용

--- codingTest/BOJ/기초2/브루트포스/3085-사탕게임.py
+++ codingTest/BOJ/기초2/브루트포스/3085-사탕게임.py
@@ -1,20 +1,30 @@
-'''
-문제
-상근이는 어렸을 적에 "봄보니 (Bomboni)" 게임을 즐겨했다.
+import sys
+N= int(sys.stdin.readline()) 
+M = [ list(sys.stdin.readline().rstrip()) for i in range(N)]
+dx,dy = [-1, 0], [0, -1]
+def ck(x,y):
+  tmp = 0
+  count_x, count_y = 1, 1
+  for j in range(1, N):
+    if M[x][j] == M[x][j-1]: count_x+=1
+    else: 
+      if tmp < count_x: tmp = count_x
+      count_x = 1
+    if M[j][y] == M[j-1][y]: count_y+=1
+    else: 
+      if tmp < count_y: tmp = count_y
+      count_y = 1 # 초기화를... 해줘야지.. ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
+  return max(tmp, count_x, count_y)
 
-가장 처음에 N×N크기에 사탕을 채워 놓는다. 사탕의 색은 모두 같지 않을 수도 있다. 상근이는 사탕의 색이 다른 인접한 두 칸을 고른다. 그 다음 고른 칸에 들어있는 사탕을 서로 교환한다. 이제, 모두 같은 색으로 이루어져 있는 가장 긴 연속 부분(행 또는 열)을 고른 다음 그 사탕을 모두 먹는다.
+result = []
 
-사탕이 채워진 상태가 주어졌을 때, 상근이가 먹을 수 있는 사탕의 최대 개수를 구하는 프로그램을 작성하시오.
-
-입력
-첫째 줄에 보드의 크기 N이 주어진다. (3 ≤ N ≤ 50)
-
-다음 N개 줄에는 보드에 채워져 있는 사탕의 색상이 주어진다. 빨간색은 C, 파란색은 P, 초록색은 Z, 노란색은 Y로 주어진다.
-
-사탕의 색이 다른 인접한 두 칸이 존재하는 입력만 주어진다.
-
-출력
-첫째 줄에 상근이가 먹을 수 있는 사탕의 최대 개수를 출력한다.
-
-'''
-# 이해가 필요함..
+for i in range(N*N):
+  x = i // N
+  y = i % N
+  for w in range(2):
+    xx, yy = x+dx[w], y+dy[w]
+    if xx < 0 or xx == N or yy<0 or yy == N: continue
+    M[x][y], M[xx][yy] = M[xx][yy], M[x][y]
+    result.append(max(ck(x,y), ck(xx,yy)))
+    M[x][y], M[xx][yy] = M[xx][yy], M[x][y]
+print(max(result))
