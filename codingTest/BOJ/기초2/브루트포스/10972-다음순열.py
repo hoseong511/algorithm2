537571992d69b9d53ea308f4800630466d9b3ccb
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-16 21:37:23 +0900

    Update BOJ: 알고리즘기초2,  10972..., dfs 실패

---
+++ codingTest/BOJ/기초2/브루트포스/10972-다음순열.py
@@ -0,0 +1,50 @@
+import sys
+sys.setrecursionlimit(10000)
+
+def dfs(N, r, depth):
+  global ck
+  if depth == r :
+    lake.append(result[::])
+    if result == data:
+      ck = True
+      return
+    if ck: 
+      print(*result)
+      exit(0)
+  for i in range(1, N+1):
+    if not visited[i]:
+      visited[i] = True
+      result.append(i)
+      dfs(N, r, depth+1)
+      result.pop()
+      visited[i] = False
+  return
+
+
+
+
+def permutation(arr, r):
+  result = []
+  def permute(p=[], index=[]):
+    if len(p) == r:
+      result.append(p)
+      return
+    for idx, data in enumerate(arr):
+      if idx not in index:
+        permute(p + [data], index + [idx])
+  permute()
+  return result
+
+
+result, lake = [], []
+N, data = int(input()), list(map(int,input().split()))
+r = len(data)
+ck = False
+N = [i for i in range(1, N+1)]
+for r in permutation(N, r):
+  if ck:
+    print(*r)
+    exit(0)
+  if data == r :
+    ck = True
+print(-1)
