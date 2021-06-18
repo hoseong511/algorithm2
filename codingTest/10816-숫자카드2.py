29392736e855c5b62a4d8f76924a4934fcc078e1
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-18 22:39:13 +0900

    UPdate BOJ: 10866, 덱

--- codingTest/10816-숫자카드2.py
+++ codingTest/10816-숫자카드2.py
@@ -1,19 +1,10 @@
-from collections import deque
-import sys
-from timeit import default_timer as timer
-from datetime import timedelta
-
-start = timer()
-input = sys.stdin.readline
 N = int(input())
-# deq = input().split()
-deq = [i for i in range(1, 500001)]
-M = input()
+data = input().split()
+M = int(input())
 ck = input().split()
-ck = [ 250000 for _ in range(20000)]
 table = {}
-while deq:
-  target = deq.pop()
+while data:
+  target = data.pop()
   try:
     table[target]+=1
   except:
@@ -23,7 +14,3 @@ for i in ck:
     print(table[i], end=' ')
   except:
     print(0, end=' ')
-
-end = timer()
-print()
-print(timedelta(seconds=end-start))
