6d62f694aee4e0b493b2fe5009265bf5983cadb6
hoseong511 <62678380+hoseong511@users.noreply.github.com>
2021-06-18 18:31:53 +0900

    Updaet BOJ:1018, 브루트포스, 방향벡터x

--- codingTest/BOJ/기초2/브루트포스/1018-체스판다시칠하기.py
+++ codingTest/BOJ/기초2/브루트포스/1018-체스판다시칠하기.py
@@ -1,31 +1,21 @@
+def ck_wb(a, b):
+  ret_w, ret_b = 0, 0
+  for i in range(64):
+    x = i // 8 + a
+    y = i % 8 + b
+    if (x+y) % 2 == 0:
+      if Map[x][y] != 'W': ret_w+=1
+      if Map[x][y] != 'B': ret_b+=1 
+    else:
+      if Map[x][y] != 'B': ret_w+=1
+      if Map[x][y] != 'W': ret_b+=1
+  return [ret_w, ret_b]
+  
 N, M = map(int, input().split())
 Map = [list(input()) for _ in range(N)]
-
-dx, dy = [0, -1, 0, 1], [1, 0, -1, 0]
-arr=[[0]*(M-8+1) for _ in range(N-8+1)]
-for a in range(N-8+1):
-  for b in range(M-8+1):
-    ck = [ [False for _ in range(M)] for _ in range(N)]
-    Map1 = [ m[:] for m in Map]
-    ret = 0
-    for i in range(a, a+8):
-      for j in range(b, b+8):
-        for w in range(4):
-          xx, yy = i + dx[w], j + dy[w]
-          if xx < a or yy < b or xx == a+8 or yy == b+8:
-            continue
-          if ck[xx][yy] or Map1[i][j] != Map1[xx][yy]:
-            continue
-          if Map1[i][j] == 'B':
-            Map1[xx][yy] = 'W'
-            ret += 1
-          elif Map1[i][j] == 'W':
-            Map1[xx][yy] = 'B'
-            ret += 1
-        ck[i][j] = True
-    arr[a][b]=ret      
 min_num = 2500
-for a in arr:
-  min_num = min([min_num] +a)
+for a in range(N-7):
+  for b in range(M-7):
+    data = ck_wb(a,b)
+    min_num= min([min_num] +data)
 print(min_num)
-
