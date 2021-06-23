0096e7699ae879fafc2069eeca379ad4670db4c3
hoseong511 <thdghtjd115@naver.com>
2021-06-24 02:49:04 +0900

    Update BOJ: 1654, 이분탐색

---
+++ codingTest/BOJ/기초2/이진탐색/1654-랜선자르기.py
@@ -0,0 +1,16 @@
+K, N = map(int, input().split())
+data = [ int(input()) for _ in range(K)]
+max_target = 2147483647
+min_target = 1
+
+lan= []
+while max_target!=min_target:
+  count_lan =0
+  tmp = (max_target + min_target+1) // 2
+  for i in data:
+    count_lan += i // tmp
+  if count_lan < N :
+    max_target = tmp-1
+  elif count_lan >= N:
+    min_target = tmp
+print(max_target)
