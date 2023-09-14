ddd88724eefd65631afecc77723dadecf3b95e17
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-14 17:48:27 +0900

    Time: 50 ms (49.38%), Space: 10.6 MB (49.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,10 @@ public:
 	vector<int> twoSum(vector<int>& nums, int target) {
 		vector<vector<int>> res;
 		for (int i = 0; i < static_cast<int>(nums.size()) - 1; i++) {
-			for (int j = i + 1; j < static_cast<int>(nums.size()); j++) {
-				if (nums[i] + nums[j] == target)
-					return vector<int>{i, j};
-			}
+			int j = target - nums[i];
+			auto it = find(nums.begin() + i + 1, nums.end(), j);
+			if (it != nums.end())
+				return vector<int>{i, static_cast<int>(it - nums.begin())};
 		}
 		return vector<int>{};
 	}
