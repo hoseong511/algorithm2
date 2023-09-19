f176d3130d262a6b989bba51ffc7f643dc55612a
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-19 18:03:09 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (6.21%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.cpp
@@ -11,11 +11,23 @@
 class Solution {
 public:
 	ListNode* swapPairs(ListNode* head) {
-		ListNode* tar = head;
+		ListNode* tar = head, *prev = 0, *n1, *n2, *tmp;
 		while (head && head->next) {
-			swap(head->val, head->next->val);
-			head = head->next->next;
+			n1 = head;
+			n2 = head->next;
+			tmp = n2->next;
+			n1->next = n2->next;
+			n2->next = n1;
+			if (!prev) {
+				prev = n1;
+				tar = n2;
+			} else {
+				prev->next = n2;
+				prev = n1;
+			}
+			head = tmp;
 		}
 		return tar;
 	}
 };
+
