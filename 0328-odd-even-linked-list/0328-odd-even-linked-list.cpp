b991bc2c415291cff06d75c9868c0327b0e830b0
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-20 14:58:15 +0900

    Time: 3 ms (99.23%), Space: 10.8 MB (13.04%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,35 +11,19 @@
 class Solution {
 public:
 	ListNode* oddEvenList(ListNode* head) {
-		ListNode* odd = 0, *odd_cur = 0, *even = 0, *even_cur = 0, *tmp;
-		int idx = 1;
-		while (head) {
-			tmp = head->next;
-			int odd_n = idx % 2;
-			if (odd_n) {
-				if (!odd_cur) {
-					odd_cur = head;
-					odd = head;
-				} else {
-					odd_cur->next = head;
-					odd_cur = odd_cur->next;
-				}
-			} else {
-				if (!even_cur) {
-					even_cur = head;
-					even = head;
-				} else {
-					even_cur->next = head;
-					even_cur = even_cur->next;
-				}
-			}
-			head = tmp;
-			idx++;
+		ListNode *odd_cur = 0, *even = 0, *even_cur = 0;
+		if (!head)
+			return 0;
+		odd_cur = head;
+		even_cur = head->next;
+		even = head->next;
+		while (even_cur && even_cur->next) {
+			odd_cur->next = odd_cur->next->next;
+			even_cur->next = even_cur->next->next;
+			odd_cur = odd_cur->next;
+			even_cur = even_cur->next;
 		}
-		if (odd_cur && even_cur) {
-			odd_cur->next = even;
-			even_cur->next = 0;
-		}
-		return odd ? odd : even;
+		odd_cur->next = even;
+		return head;
 	}
 };
