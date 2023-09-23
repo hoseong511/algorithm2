0d60f698cc3c2d775d054c5863520b426e885319
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-23 17:57:50 +0900

    Time: 3 ms (92.15%), Space: 19.2 MB (7.34%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,6 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+TreeNode* init_node(string val) {
+	TreeNode* node;
+	int num = val == "null" ? -101 : atoi(val.c_str());
+	if (num == -101)
+		return nullptr;
+	node = new TreeNode(num);
+	return node;
+}
+
+TreeNode* add_node(TreeNode* root, TreeNode* node, queue<TreeNode*>& q, int idx) {
+	if (root == NULL) {
+		root = node;
+	} else if (q.front() == nullptr) {
+		q.pop();
+	} else if (q.front()->left == nullptr && idx % 2 == 1) {
+		q.front()->left = node;
+	} else {
+		q.front()->right = node;
+		q.pop();
+	}
+	q.push(node);
+	return root;
+}
+
+TreeNode* mktree(vector<string>& lst) {
+	TreeNode* root = 0;
+	queue<TreeNode*> q;
+	for (size_t i = 0; i < lst.size(); i++) {
+		TreeNode* node = init_node(lst[i]);
+		root = add_node(root, node, q, i);
+	}
+	return root;
+}
+
+ 
+
 class Solution {
 public:
 	int maxDepth(TreeNode* root) {
