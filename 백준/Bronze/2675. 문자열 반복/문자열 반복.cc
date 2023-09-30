68d96d5ab87aeaba0c7af071a0b81a1af2269834
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-30 15:42:09 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,20 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int T, R;
+
+	scanf("%d", &T);
+	for (int i = 0; i < T; i++) {
+		char str[21] = {0,};
+		scanf("%d %s", &R, str);
+		for (auto x: str) {
+			printf("%s", string(R, x).c_str());
+		}
+		printf("\n");
+	}
+	return 0;
+}
