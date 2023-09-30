c6f9bd3c614eef0530913029a05618b6f1c731e6
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-09-30 17:23:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.cc
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	int alph[26];
+	char spac[2] = {0, 0};
+	char input[101] = {0, };
+	memset(alph, -1, sizeof(alph));
+	scanf("%s", input);
+
+	for (int i = 0; input[i] != 0; i++) {
+		if (alph[input[i] - 'a'] == -1)
+			alph[input[i] - 'a'] = i;
+	}
+	for (auto a : alph) {
+		printf("%s%d", spac, a);
+		spac[0] = ' ';
+	}
+	printf("\n");
+	return 0;
+}
