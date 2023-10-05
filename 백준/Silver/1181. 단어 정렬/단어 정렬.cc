b8ce1666bdc8388191fd7216cc353bd4ce565d3b
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-10-05 14:15:24 +0900

    [Silver V] Title: 단어 정렬, Time: 48 ms, Memory: 4932 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+
+using namespace std;
+
+
+struct Compare{
+	bool operator() (string a, string b) const {
+		if (a.length() == b.length())
+			return a < b;
+		else
+			return a.length() < b.length();
+	}
+};
+
+int main()
+{
+	FAST;
+	int N, min = 51, idx = -1;
+	set<string, Compare> data;
+	string input;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> input;
+		data.insert(input);
+	}
+	for (auto x : data)
+		cout << x << '\n';
+	return 0;
+}
