6b7ee24a01cc1e0860021ef1048bff285fe470ac
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-10-05 19:39:49 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 29452 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -0,0 +1,10 @@
+import sys
+N = int(sys.stdin.readline())
+memo = [[0,0,0] for _ in range(1001)]
+data = [0]+[list(map(int,sys.stdin.readline().split())) for _ in range(N)] 
+memo[1] = data[1]
+for i in range(2, N+1):
+  memo[i][0] = data[i][0] + min(memo[i-1][1], memo[i-1][2])
+  memo[i][1] = data[i][1] + min(memo[i-1][0], memo[i-1][2])
+  memo[i][2] = data[i][2] + min(memo[i-1][0], memo[i-1][1])
+print(min(memo[N]))
