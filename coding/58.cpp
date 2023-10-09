b05a65eddd2c5c8cc6e100f2b47819ffe254fb58
hoseong511 <thdghtjd115@naver.com>
2023-10-09 19:49:29 +0900

    leetcode : 58.cpp list sort

---
+++ coding/58.cpp
@@ -0,0 +1,43 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+	void search(ListNode* head)
+	{
+		ListNode* fast = head;
+		ListNode* slow = head;
+		while (fast && fast->next) {
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+	}
+    ListNode* sortList(ListNode* head) {
+		ListNode* node;
+		return node;
+    }
+};
+
+void add_node(ListNode* lst, int val)
+{
+	ListNode* node;
+	node = new ListNode(val);
+	while (lst && lst->next)
+		lst = lst->next;
+	lst->next = node;
+}
+
+int main()
+{
+	Solution s;
+	ListNode lst(4);
+	add_node(&lst, 2);
+	add_node(&lst, 1);
+	add_node(&lst, 3);
+	return 0;
+}
--- coding/b49.cpp
+++ coding/b49.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <limits.h>
 #define FAST ios::sync_with_stdio(false); cin.tie(NULL);
 
@@ -8,29 +9,40 @@ using namespace std;
 class Solution {
 	public:
 		vector<string> map;
+		vector<string> chk;
+		vector<vector<int>> ans;
 		int N, M;
-		int min = INT_MAX;
+		int dx[4] = {0, 0, 1, -1};
+		int dy[4] = {1, -1, 0, 0};
+
 		void init() {
 			cin >> N >> M;
 			map.assign(N, "");
 			for (int i = 0; i < N; i++)
 				cin >> map[i];
+			chk = map;
+			ans.assign(N, vector<int>(M, 1));
 		}
-		void dfs(int i, int j, int cnt) {
-			if (i == N - 1 && j == M -1) {
-				cnt++;
-				if (min > cnt)
-					min = cnt;
-				return ;
-			} else if ((i < 0 || i > N - 1) || (j < 0 || j > M - 1)) {
-				return ;
-			} else if (map[i][j] != '1') {
-				return ;
+		void bfs(int i, int j) {
+			queue<pair<int, int>> q;
+			q.push({i, j});
+			chk[i][j] = '0';
+			while (!q.empty()) {
+				int x = q.front().first;
+				int y = q.front().second;
+				for (int k = 0; k < 4; k++) {
+					int next_x = x + dx[k];
+					int next_y = y + dy[k];
+					if ((0 <= next_x && next_x < N) && (0 <= next_y && next_y < M)) {
+						if (map[next_x][next_y] == '1' && chk[next_x][next_y] == '1') {
+							chk[next_x][next_y] = '0';
+							ans[next_x][next_y] = ans[x][y] + 1;
+							q.push({next_x, next_y});
+						}
+					}
+				}
+				q.pop();
 			}
-			map[i][j] = '0';
-			cnt++;
-			dfs(i, j + 1, cnt);
-			dfs(i + 1, j, cnt);
 		}
 };
 
@@ -40,7 +52,7 @@ int main()
 	Solution s;
 
 	s.init();
-	s.dfs(0, 0, 0);
-	cout << s.min << '\n';
+	s.bfs(0, 0);
+	cout << s.ans[s.N -1][s.M - 1] << '\n';
 	return 0;
 }
