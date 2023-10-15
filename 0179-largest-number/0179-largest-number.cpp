4217693b805b1c50009a7f70ba0ed558e711048d
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-10-15 23:21:24 +0900

    Time: 27 ms (5.43%), Space: 11.4 MB (75.15%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    string largestNumber(vector<int>& nums) {
+    string largestNumber(vector<int>& lst) {
 		string ans;
-        sort(nums.begin(), nums.end(), [](long long a, long long b) {
-			string s1 = to_string(a);
-			string s2 = to_string(b);
-			return stoull(s1 + s2) > stoull(s2 + s1);
-		});
-		for (auto x : nums) {
-            if (ans == "0")
-                    break;
+		int i = 1;
+		while (i < lst.size()) {
+			int j = i;
+			while (j > 0) {
+				string s1 = to_string(lst[j]);
+				string s2 = to_string(lst[j-1]);
+				if (stoull(s1 + s2) > stoull(s2 + s1))
+					swap(lst[j], lst[j - 1]);
+				j--;
+			}
+			i++;
+		}
+		for (auto x : lst) {
+			if (ans == "0")
+				break;
 			ans += to_string(x);
 		}
 		return ans;
-    }
+	}
 };
