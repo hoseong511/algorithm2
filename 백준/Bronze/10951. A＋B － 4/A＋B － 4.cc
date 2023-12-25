01e2c91dbeaea6dba7bb652122f3e4e8f6cb5e90
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-26 00:08:54 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main()
+{
+	FAST;
+	int a, b;
+	vector<int> lst;
+	while (true) {
+		cin >> a >> b;
+		if (cin.eof() == true)
+			break ;
+		lst.push_back(a+b);
+	}
+	for (auto x: lst)
+		cout << x << '\n';
+	return 0;
+}
