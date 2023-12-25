d2d1ad244d2abb285ae2b982fb4a8c16d4a2225a
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-26 01:17:07 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main()
+{
+	FAST;
+	int T, a, b;
+	cin >> T;
+	for (int i = 1; i < T + 1; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i << ": "
+		<< a << " + " << b << " = " << a + b << '\n';
+	}
+	return 0;
+}
