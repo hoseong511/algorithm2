f609ed391715d570dee39b4b9191ecc96f4de29b
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-25 23:23:35 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main()
+{
+	FAST;
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << '\n';
+	return 0;
+}
