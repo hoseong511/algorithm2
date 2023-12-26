e0af948c861c06f39ff3dad18f5eeb58321a1870
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-26 23:02:29 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main()
+{
+	FAST;
+	int N;
+	cin >> N;
+	for (int i = 1; i < N + 1; i++) {
+		cout << i << '\n';
+	}
+	return 0;
+}
