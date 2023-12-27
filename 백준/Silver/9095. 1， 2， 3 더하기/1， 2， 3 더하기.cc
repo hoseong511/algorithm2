5fd1d55c3bc9cbe12d5823054c62e2056eb623ca
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-28 01:13:42 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -1,23 +1,21 @@
 #include <iostream>
 #define FAST ios::sync_with_stdio(false); cin.tie(NULL);
-
 using namespace std;
-
 int main()
 {
 	FAST;
-	int N, input;
+	int N, T;
 	int arr[11] = {0, };
+	cin >> T;
 	arr[1] = 1;
 	arr[2] = 2;
 	arr[3] = 4;
-	for (int i = 4; i < 11; i++) {
+	for (int i = 4; i <= 10; i++) {
 		arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
 	}
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> input;
-		cout << arr[input] << '\n';
+	for (int i = 0; i < T; i++) {
+		cin >> N;
+		cout << arr[N] << '\n'; 
 	}
 	return 0;
 }
