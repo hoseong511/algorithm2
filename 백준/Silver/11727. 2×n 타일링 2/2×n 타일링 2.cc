d28ebd70b86c3aff11733421a21ad7a882cbec9a
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-28 00:35:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main()
+{
+	FAST;
+	int N;
+	int arr[1001] = {0, };
+	cin >> N;
+	arr[1] = 1;
+	arr[2] = 3;
+	for (int i = 3; i <= N; i++) {
+		arr[i] = (arr[i - 1] + 2 * arr[i - 2]) % 10007;
+	}
+	cout << arr[N]; 
+	return 0;
+}
