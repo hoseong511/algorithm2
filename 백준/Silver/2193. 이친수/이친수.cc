8e129f6a0c5b123afd7011dbf8ef5c8859dbbf1c
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2023-12-30 16:10:41 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.cc
+++ 백준/Silver/2193. 이친수/이친수.cc
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
 	int N;
-	long long arr[91][2] = {{0,}, {0, 1}, };
+	long long arr[2] = {0, 1};
 	cin >> N;
 	for (int i = 2; i <= N; i++) {
-		arr[i][0] = arr[i-1][1] + arr[i-1][0];
-		arr[i][1] = arr[i-1][0];
+		long long tmp = arr[0];
+		arr[0] = arr[0] + arr[1];
+		arr[1] = tmp;
 	}
-	cout << arr[N][0] + arr[N][1];
+	cout << arr[0] + arr[1];
 	return 0;
 }
