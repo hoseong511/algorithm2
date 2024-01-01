d46084d6433de40515609a67a99e505809ca4a2a
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-01 12:30:19 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+inline int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int main()
+{
+	FAST;
+	int N;
+	int arr[1001] = {0, };
+	int p[1001] = {0, };
+	fill(p + 1, p + 1001, 1);
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> arr[i];
+	}
+	int res = p[1];
+	for (int i = 2; i <= N; i++)
+	{
+		for (int j = i - 1; j >= 1; j--)
+		{
+			if (arr[i] > arr[j])
+			{
+				p[i] = max(p[i], p[j] + 1);
+			}
+		}
+		res  = max(p[i], res);
+	}
+	cout << res << '\n';
+	return 0;
+}
