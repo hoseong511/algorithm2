049b25c57fa51c9b49c5e013a193ef36474ee92f
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-03 03:23:53 +0900

    [Gold V] Title: 암호코드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.cc
+++ 백준/Gold/2011. 암호코드/암호코드.cc
@@ -1,25 +1,34 @@
 #include <iostream>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL);
 #define MOD 1000000
 using namespace std;
-int dp[5001]={1,1, };
-int solution(string code, int n ) {
-	if(code[0]=='0') return 0;
-	
-	for(int i=2;i<=n;i++){
-		if(code[i-1]!='0') dp[i]=dp[i-1]%MOD;
-		
-		int tmp=(code[i-2]-'0')*10+(code[i-1]-'0');
-		if(tmp>=10&&tmp<=26)
-			dp[i]=(dp[i]+dp[i-2])%MOD;
+
+int main()
+{
+	FAST;
+	string input;
+	int p[5001] = {1, 1,};
+	int a[5001] = {0, };
+	int N;
+	cin >> input;
+	N = input.length();
+	for (int i = 1; i <= N; i++)
+	{
+		a[i] = input[i-1] - '0';
 	}
-
-    return dp[n];
-}
-
-int main() {
-    string str;  
-    cin >> str;   
-    int answer = solution(str, str.length());  
-    cout << answer;
-    return 0;
+	if (input[0] == '0')
+	{
+		cout << 0 << '\n';
+		return 0;
+	}
+	for (int i = 2; i <= N; i++)
+	{
+		if (a[i] >= 1 && a[i] <= 9)
+			p[i] = (p[i - 1] + p[i]) % MOD;
+		int num = a[i] + a[i-1] * 10;
+		if (num >= 10 && num <= 26)
+			p[i] = (p[i - 2] + p[i]) % MOD;
+	}
+	cout << p[N];
+	return 0;
 }
