c5a205f34e7a1f67ad25394193ad04c03708b223
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-04 23:43:58 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -1,39 +1,37 @@
-#include <cstdio>
-#include <string>
+#include <iostream>
 #include <vector>
-
+#include <algorithm>
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma optimize("unroll-loops")
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
-
 int main()
 {
-	int T;
-	vector<int> stack;
-	scanf("%d", &T);
-	for (int i = 0; i < T; i++) {
-		char input[6] = {0,};
-		int n = 0;
-		scanf("%s", input);
-		string cmd(input);
+	FAST;
+	int N;
+	vector<int> v;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int input;
+		string cmd;
+		cin >> cmd;
 		if (cmd == "push") {
-			scanf("%d", &n);
-			stack.push_back(n);
-		} else if (cmd == "pop") {
-			if (stack.empty()) {
-				printf("%d\n", -1);
-				continue ;
-			}
-			printf("%d\n", stack.back());
-			stack.pop_back();
-		} else if (cmd == "size") {
-			printf("%lu\n", stack.size());
-		} else if (cmd == "empty") {
-			printf("%d\n", stack.empty());
+			cin >> input;
+			v.push_back(input);
 		} else if (cmd == "top") {
-			if (stack.empty()) {
-				printf("%d\n", -1);
-				continue ;
+			cout << (!v.empty() ? v.back() : -1) << '\n';
+		} else if (cmd == "size") {
+			cout << v.size() << '\n';
+		} else if (cmd == "empty") {
+			cout << (v.empty() ? 1 : 0) << '\n';
+		} else if (cmd == "pop") {
+			if (!v.empty()) {
+				cout << v.back() << '\n';
+				v.pop_back();
+				continue;
 			}
-			printf("%d\n", stack.back());
+			cout << -1 << '\n';
 		}
 	}
 	return 0;
