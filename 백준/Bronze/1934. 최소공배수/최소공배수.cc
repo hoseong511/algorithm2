e22002b2654af5047a9e1cd94285c4c67ed1313a
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-09 22:55:52 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma optimize("unroll-loops")j
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int gcd(int a, int b)
+{
+	int c;
+	while (b) {
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+int main()
+{
+	FAST;
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int A, B;
+		int g, l;
+		cin >> A >> B;
+		g = gcd(A, B);
+		l = A * B / g;
+		cout << l << '\n';
+	}
+	return 0;
+}
