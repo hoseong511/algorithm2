ecfa4dbd90601bb56f0fdad1837e556eee44d190
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-11 01:11:49 +0900

    [Bronze II] Title: 8진수 2진수, Time: 12 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("Ofast")
+// #pragma optimize("unroll-loops")j
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+int main()
+{
+	FAST;
+	string N;
+	string v;
+	cin >> N;
+	int len = N.length();
+	for (int i = 0; i < len; i++) {
+		int c;
+		int num = N[i] - '0';
+		char bit[4] = {'0','0','0', 0};
+		for (int j = 0; j < 3; j++) {
+			c = num % 2;
+			if (c) bit[2 - j] = '1';
+			num = num / 2;
+		}
+		if (i == 0) v.append(to_string(atoi(bit)));
+		else v.append(bit);
+	}
+	cout << v;
+	return 0;
+}
