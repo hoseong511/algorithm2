df44f1d189171de4fc3add6372996f916fcd65cb
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-11 22:47:31 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.cc
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -9,19 +9,11 @@ int main()
 {
 	FAST;
 	int N;
-	vector<int> v;
 	cin >> N;
-	v.assign(N + 1, 0);
-	for (int i = 2; i < sqrt(N + 1); i++) {
-		if (v[i] == 1) continue ;
-		for (int j = 2; i * j < N + 1; j++) {
-			v[i * j] = 1;
-		}
-	}
 	while (N != 1) {
 		int i;
 		for (i = 2; i < N + 1; i++) {
-			if (v[i] == 0 && N % i == 0) {
+			if (N % i == 0) {
 				cout << i << '\n';
 				break ;
 			}
