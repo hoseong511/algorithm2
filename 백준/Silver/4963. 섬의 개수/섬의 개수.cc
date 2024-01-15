c73a71dde769bdfc2b988d1a2770f9e345a8d1b6
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-16 01:36:38 +0900

    [Silver II] Title: 섬의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.cc
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.cc
@@ -6,18 +6,18 @@
 #define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 using namespace std;
 int w, h;
-vector<string> g;
-vector<string> chk;
+bool g[50][50];
+bool chk[50][50];
 int dfs(int y, int x, int cnt)
 {
 	char dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
 	char dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
 	if (y < 0 || y >= h || x < 0 || x >= w)
 		return cnt;
-	if (chk[y][x] == '1')
+	if (chk[y][x] == 1)
 		return cnt;
-	chk[y][x] = '1';
-	if (g[y][x] == '1') {
+	chk[y][x] = 1;
+	if (g[y][x] == 1) {
 		cnt += 1;
 		for (int i = 0; i < 8; i++) {
 			cnt = dfs(y + dy[i], x + dx[i], cnt);
@@ -33,19 +33,16 @@ int main()
 		cin >> w >> h;
 		if (w == 0 && h == 0)
 			break ;
-		g.clear();
-		g.assign(h, string(w, ' '));
-		chk.clear();
-		chk.assign(h, string(w, '0'));
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
 				cin >> g[i][j];
+                chk[i][j] = 0;
 			}
 		}
 		int cnt = 0;
 		for (int i = 0; i < h; i++) {
 			for (int j = 0; j < w; j++) {
-				if (chk[i][j] == '1')
+				if (chk[i][j] == 1)
 					continue ;
 				if (dfs(i, j, 0))
 					cnt++;
