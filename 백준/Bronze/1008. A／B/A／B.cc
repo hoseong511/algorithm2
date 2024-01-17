56559bc162f00d3ba6f03936a7795a28820f74f4
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-17 22:40:37 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <queue>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+#define MAX 1001
+int main()
+{
+	double A, B;
+	cin >> A >> B;
+	cout.precision(10);
+	cout << A / B;
+	return 0;
+}
