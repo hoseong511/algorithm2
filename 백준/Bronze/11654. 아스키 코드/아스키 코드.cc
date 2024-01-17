d278369a30c91385745b13ec2bbdb72ef00b483e
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-17 23:01:57 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+int main()
+{
+	char A;
+	cin >> A;
+	cout << static_cast<int>(A);
+	return 0;
+}
