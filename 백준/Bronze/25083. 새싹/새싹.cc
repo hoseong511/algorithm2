18815aa5185c2e5ebf58f205dab33b7674d8e161
Hoseong Song <62678380+hoseong511@users.noreply.github.com>
2024-01-17 23:09:40 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.cc
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -1,11 +1,14 @@
 #include <iostream>
-
+#define FAST ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+using namespace std;
 int main()
 {
-	std::cout << "         ,r'\"7" << '\n'
-		 <<  "r`-_   ,'  ,/" << '\n'
-		 <<  " \\. \". L_r'" << '\n'
-		 <<  "   `~\\/" << '\n'
-		 <<  "      |" << '\n'
-		 <<  "      |" << std::endl;
+	cout 
+	<< "         ,r'\"7" << "\n"
+	<< "r`-_   ,'  ,/" << "\n"
+	<< " \\. \". L_r'" << "\n"
+	<< "   `~\\/" << "\n"
+	<< "      |" << "\n"
+	<< "      |" << "\n";
+	return 0;
 }
